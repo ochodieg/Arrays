aa72b7d1a3228f32f8a9bb0beddb3287040d5cb6
Ivan <the_real_iori_orochi@hotmail.com>
2020-09-09 08:24:50 -0600

    Add project files.

---
+++ Arrays.vb
@@ -0,0 +1,19 @@
+﻿Module Arrays
+
+    Sub Main()
+        Simplearray()
+    End Sub
+    Sub Simplearray()
+
+        Dim names(5) As String
+
+        names(0) = "Bob"
+        names(3) = "Jimmy"
+
+        Console.WriteLine("hello " & names(3))
+
+        Console.Read()
+
+    End Sub
+
+End Module
---
+++ My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("Arrays")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("Arrays")>
+<Assembly: AssemblyCopyright("Copyright ©  2020")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("8842f33a-af5d-49c0-82a4-e148a3ea2b0d")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")>
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
